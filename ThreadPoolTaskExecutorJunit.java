260fb8d2d60234601d5986196f5e76db2b860c04
Jain-Pratiksha <74486549+Jain-Pratiksha@users.noreply.github.com>
2023-11-03 11:48:15 +0530

    Create ThreadPoolTaskExecutorJunit.java
    The code explains how to mock ThreadPoolTaskExecutor using mockito.

---
+++ ThreadPoolTaskExecutorJunit.java
@@ -0,0 +1,19 @@
+// Mockito Junit for ThreadPoolTaskExecutor
+
+// To Mock
+
+// @Autowired
+// @Qualifier("executor")
+// Executor taskexecutor;
+
+// ((ThreadPoolTaskExecutor)taskexecutor).getThreadPoolExecutor()
+
+
+// Junit
+
+@Mock
+private ThreadPoolTaskExecutor ThreadPoolTaskExecutor;
+
+ThreadPoolTaskExecutor executor = getThreadPoolTaskExecutor();
+        when(ThreadPoolTaskExecutor.getThreadPoolExecutor())
+                .thenReturn(executor.getThreadPoolExecutor());
